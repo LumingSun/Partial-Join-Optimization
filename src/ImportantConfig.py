fe0ada98c3c541395ecf4506c24587dc9533cd1c
LumingSun <luming_s@126.com>
2022-04-05 19:30:46 +0800

    add: pg config

--- src/ImportantConfig.py
+++ src/ImportantConfig.py
@@ -1,3 +1,5 @@
+import psycopg2
+
 class Config:
     def __init__(self,):
         self.sytheticDir = "Queries/synthetic"
@@ -9,3 +11,21 @@ class Config:
         self.ip = "127.0.0.1"
         self.port = 5432
 
+class PGConfig:
+    def __init__(self):
+        self.keepExecutedPlan =True
+        self.maxTimes = 5
+        self.maxTime = 300000
+class PGRunner:
+    def __init__(self,dbname = '',user = '',password = '',host = '',port = ''):
+        self.con = psycopg2.connect(database=dbname, user=user,
+                               password=password, host=host, port=port)
+
+    def optimizer_cost(self, query):
+        query = "EXPLAIN (FORMAT JSON) " + query + ";"
+        cursor = self.con.cursor()
+        settings = "set max_parallel_workers_per_gather = 0; "
+        cursor.execute(settings+query)
+        rows = cursor.fetchone()
+        cursor.close()
+        return rows[0][0]
